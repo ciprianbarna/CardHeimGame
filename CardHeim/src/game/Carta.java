78efb55cc7e13197bc287dc58e8788c0a63f8e74
Nacho <ignaciolopezgarcia_97@hotmail.com>
2019-09-02 19:06:00 +0200

    Código de la clase carta, donde ubico los atributos nombre y efecto y sus respectivos métodos de get/set junto con el constructor de la clase para esos atributos

--- CardHeim/src/game/Carta.java
+++ CardHeim/src/game/Carta.java
@@ -1,4 +1,26 @@
 package game;
 
 public abstract class Carta {
+    // Atributos
+    private    String name, efecto;
+
+    // Métodos
+    /** Constructor
+     * @param nombre - nombre de la carta
+     * @param efecto - descripción de la carta
+     */
+    public Carta(String nombre, String efecto)
+    {
+        this.name = name;
+        this.efecto = efecto;
+    }
+
+    // Métodos get
+    public String getName(){ return name;}
+    public String getEfecto(){return efecto;}
+
+    // Métodos set
+    public void setName(String name){ this.name = name; }
+    public void setEfecto(String efecto){ this.efecto = efecto;}
+
 }
